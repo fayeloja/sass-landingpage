d09e2cbee83e7513f2bb004c633802f88fa78419
fayeloja <fayeloja@gmail.com>
2025-02-11 19:22:39 +0100

    Initial commit for creating a Saas landing page

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import video1 from '../assets/video1.mp4';
+import video2 from '../assets/video1.mp4';
+
+const HeroSection = () => {
+  return (
+    <div>HeroSection</div>
+  )
+}
+
+export default HeroSection;
